8787b5858cdad80ce1614ea31fbc5e21a84d0537
James Jalandoni <jamesjalandoni1@gmail.com>
2017-08-21 13:17:31 -0700

    Add queryParam to url construction (#5)
    * Add queryParam to url construction
    
    * queryParams same API as fetchParams
    
    * Store last used queryParams
    
    * Add actions tests
    
    * Index reducer case
    
    * Test for show reducer
    
    * README copy updates
    
    * Test middleWare
    
    * Do not construct query string for keys with undefined values

--- src/apiReducer.js
+++ src/apiReducer.js
@@ -99,7 +99,7 @@ const destroyMember = ({id, state}) => {
   return state.models.filter(model => model.id !== id)
 }
 
-const setMemberLoading = ({id, cId, state}) => {
+const setMemberLoading = ({id, cId, state, queryParams}) => {
   // sets the loading state of a member within a collection
   const currentModel = findModel({id, cId, state})
 
@@ -107,7 +107,7 @@ const setMemberLoading = ({id, cId, state}) => {
     // model does not yet exist in models array -- create it.
     return collectionWithNewModel({state,
       model: createNewModel({id, cId,
-        metaData: { loading: true }
+        metaData: { loading: true, queryParams }
       })
     })
   }
@@ -115,7 +115,7 @@ const setMemberLoading = ({id, cId, state}) => {
   // model already exists in model array -- update its loading state.
   return collectionWithUpdatedModel({id, cId, state,
     updatedModel: createNewModel({id, cId,
-      metaData: { loading: true },
+      metaData: { loading: true, queryParams },
       attributes: currentModel.attributes
     }
   )})
@@ -185,12 +185,13 @@ export default (config) => {
       const resourceNameSpace = getResourceNameSpace({config, resource})
       const isSingleModel = resourceNameSpace === 'attributes'
       const idAttribute = getResourceIdAttribute({config, resource})
-
+      const { queryParams } = action.data || {}
       switch(action.type) {
         case `${resource}.INDEX`: {
           return Object.assign({}, state, createNewCollection({
             metaData: {
-              loading: true
+              loading: true,
+              queryParams
             }
           }))
         }
@@ -239,13 +240,13 @@ export default (config) => {
 
           if (isSingleModel) {
             return createNewModel({
-              metaData: { loading: true },
+              metaData: { loading: true, queryParams },
               attributes: state.attributes
             })
           }
 
           return Object.assign({}, state, {
-            models: setMemberLoading({id, state})
+            models: setMemberLoading({id, state, queryParams})
           })
         }
         case `${resource}.SHOW_SUCCESS`: {
--- test/apiReducer/indexReducerCases.spec.js
+++ test/apiReducer/indexReducerCases.spec.js
@@ -4,25 +4,50 @@ import { standardConfig } from './exampleConfigs'
 describe('INDEX actions', () => {
   const indexReducer = apiReducer(standardConfig)
   let indexReducerState = {}
+  const baseModelState = {
+    loading: false,
+    loadingError: undefined,
+  }
+  const baseCollectionState = {
+    ...baseModelState,
+    models: []
+  }
+  const baseExpectedState = {
+    Posts: baseCollectionState,
+    User: { ...baseModelState, attributes: {} }
+  }
+
+  const expectedPostsState = (newState) => ({
+    ...baseExpectedState,
+    Posts: {
+      ...baseCollectionState,
+      ...newState
+    }
+  })
+
+  it('should set queryParams on the collection', () => {
+    const queryParams = { foo: 'bar' }
+    indexReducerState = indexReducer(indexReducerState, railsActions.index({
+      resource: 'Posts',
+      queryParams
+    }))
+
+    expect(indexReducerState).toEqual(expectedPostsState({
+      loading: true,
+      queryParams
+    }))
+  })
 
   it('should set a loading state on the collection', () => {
-    indexReducerState = indexReducer(indexReducerState, railsActions.index({ resource: 'Posts' }))
+    indexReducerState = indexReducer(indexReducerState, railsActions.index({
+      resource: 'Posts',
+    }))
 
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: true,
-          loadingError: undefined,
-          models: []
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
+    expect(indexReducerState).toEqual(expectedPostsState({
+      loading: true
+    }))
   })
+
 
   it('should set a loading error state on the collection', () => {
     indexReducerState = indexReducer(indexReducerState, {
@@ -30,20 +55,9 @@ describe('INDEX actions', () => {
       error: { message: 'a bad thing happened' }
     })
 
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: false,
-          loadingError: { message: 'a bad thing happened'},
-          models: []
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
+    expect(indexReducerState).toEqual(expectedPostsState({
+      loadingError: { message: 'a bad thing happened'}
+    }))
   })
 
   it('should reset error and loading state on next INDEX call', () => {
@@ -51,20 +65,9 @@ describe('INDEX actions', () => {
       type: 'Posts.INDEX'
     })
 
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: true,
-          loadingError: undefined,
-          models: []
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
+    expect(indexReducerState).toEqual(expectedPostsState({
+      loading: true
+    }))
   })
 
   it('should set loading and collection state on success of INDEX call', () => {
@@ -73,23 +76,13 @@ describe('INDEX actions', () => {
       response: [{id: 1, foo: 'bar1'}, {id: 2, foo: 'bar2'}, {id: 3, foo: 'bar3'}]
     })
 
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: false,
-          loadingError: undefined,
-          models: [
-            {loading: false, loadingError: undefined, id: 1, attributes: {id: 1, foo: 'bar1'}},
-            {loading: false, loadingError: undefined, id: 2, attributes: {id: 2, foo: 'bar2'}},
-            {loading: false, loadingError: undefined, id: 3, attributes: {id: 3, foo: 'bar3'}}]
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
+    expect(indexReducerState).toEqual(expectedPostsState({
+      models: [
+        {loading: false, loadingError: undefined, id: 1, attributes: {id: 1, foo: 'bar1'}},
+        {loading: false, loadingError: undefined, id: 2, attributes: {id: 2, foo: 'bar2'}},
+        {loading: false, loadingError: undefined, id: 3, attributes: {id: 3, foo: 'bar3'}}
+      ]
+    }))
   })
 
   it('should replace collection state on success of subsequent INDEX call', () => {
@@ -97,24 +90,14 @@ describe('INDEX actions', () => {
       type: 'Posts.INDEX_SUCCESS',
       response: [{id: 4, foo: 'bar4'}, {id: 5, foo: 'bar5'}, {id: 6, foo: 'bar6'}]
     })
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: false,
-          loadingError: undefined,
-          models: [
-            {loading: false, loadingError: undefined, id: 4, attributes: {id: 4, foo: 'bar4'}},
-            {loading: false, loadingError: undefined, id: 5, attributes: {id: 5, foo: 'bar5'}},
-            {loading: false, loadingError: undefined, id: 6, attributes: {id: 6, foo: 'bar6'}}
-          ]
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
+
+    expect(indexReducerState).toEqual(expectedPostsState({
+      models: [
+        {loading: false, loadingError: undefined, id: 4, attributes: {id: 4, foo: 'bar4'}},
+        {loading: false, loadingError: undefined, id: 5, attributes: {id: 5, foo: 'bar5'}},
+        {loading: false, loadingError: undefined, id: 6, attributes: {id: 6, foo: 'bar6'}}
+      ]
+    }))
   })
 
   it('should correctly look for top level key matching resource name and use as data', () => {
@@ -122,24 +105,14 @@ describe('INDEX actions', () => {
       type: 'Posts.INDEX_SUCCESS',
       response: {Posts: [{id: 1, foo: 'bar1'}, {id: 2, foo: 'bar2'}, {id: 3, foo: 'bar3'}]}
     })
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: false,
-          loadingError: undefined,
-          models: [
-            {loading: false, loadingError: undefined, id: 1, attributes: {id: 1, foo: 'bar1'}},
-            {loading: false, loadingError: undefined, id: 2, attributes: {id: 2, foo: 'bar2'}},
-            {loading: false, loadingError: undefined, id: 3, attributes: {id: 3, foo: 'bar3'}}
-          ]
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
+
+    expect(indexReducerState).toEqual(expectedPostsState({
+      models: [
+        {loading: false, loadingError: undefined, id: 1, attributes: {id: 1, foo: 'bar1'}},
+        {loading: false, loadingError: undefined, id: 2, attributes: {id: 2, foo: 'bar2'}},
+        {loading: false, loadingError: undefined, id: 3, attributes: {id: 3, foo: 'bar3'}}
+      ]
+    }))
   })
 
   it('should correctly look for top level key matching lower case resource name and use as data', () => {
@@ -147,24 +120,13 @@ describe('INDEX actions', () => {
       type: 'Posts.INDEX_SUCCESS',
       response: {posts: [{id: 8, foo: 'bar8'}, {id: 9, foo: 'bar9'}, {id: 10, foo: 'bar10'}]}
     })
-    expect(indexReducerState).toEqual(
-      {
-        Posts: {
-          loading: false,
-          loadingError: undefined,
-          models: [
-            {loading: false, loadingError: undefined, id: 8, attributes: {id: 8, foo: 'bar8'}},
-            {loading: false, loadingError: undefined, id: 9, attributes: {id: 9, foo: 'bar9'}},
-            {loading: false, loadingError: undefined, id: 10, attributes: {id: 10, foo: 'bar10'}}
-          ]
-        },
-        User: {
-          loading: false,
-          loadingError: undefined,
-          attributes: {}
-        }
-      }
-    )
-  })
 
+    expect(indexReducerState).toEqual(expectedPostsState({
+      models: [
+        {loading: false, loadingError: undefined, id: 8, attributes: {id: 8, foo: 'bar8'}},
+        {loading: false, loadingError: undefined, id: 9, attributes: {id: 9, foo: 'bar9'}},
+        {loading: false, loadingError: undefined, id: 10, attributes: {id: 10, foo: 'bar10'}}
+      ]
+    }))
+  })
 })
